spylogsster/brave-core

Add DebounceService for iOS

browser/brave_content_browser_client.cc
@@ -64,7 +64,7 @@
 #include "brave/components/cosmetic_filters/browser/cosmetic_filters_resources.h"
 #include "brave/components/cosmetic_filters/common/cosmetic_filters.mojom.h"
 #include "brave/components/de_amp/browser/de_amp_throttle.h"
-#include "brave/components/debounce/browser/debounce_navigation_throttle.h"
+#include "brave/components/debounce/browser/content/debounce_navigation_throttle.h"
 #include "brave/components/decentralized_dns/content/decentralized_dns_navigation_throttle.h"
 #include "brave/components/google_sign_in_permission/google_sign_in_permission_throttle.h"
 #include "brave/components/google_sign_in_permission/google_sign_in_permission_util.h"

components/debounce/browser/core/debounce_component_installer.cc
@@ -1,9 +1,9 @@
-/* Copyright (c) 2021 The Brave Authors. All rights reserved.
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+// Copyright (c) 2023 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#include "brave/components/debounce/browser/debounce_component_installer.h"
+#include "brave/components/debounce/browser/core/debounce_component_installer.h"
 
 #include <memory>
 #include <utility>
@@ -18,7 +18,6 @@
 #include "base/types/expected.h"
 #include "brave/components/brave_component_updater/browser/dat_file_util.h"
 #include "brave/components/brave_component_updater/browser/local_data_files_service.h"
-#include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 
 using brave_component_updater::LocalDataFilesObserver;
 using brave_component_updater::LocalDataFilesService;
@@ -55,8 +54,9 @@ void DebounceComponentInstaller::OnDATFileDataReady(
   host_cache_.clear();
   rules_ = std::move(parsed_rules.value().first);
   host_cache_ = parsed_rules.value().second;
-  for (Observer& observer : observers_)
+  for (Observer& observer : observers_) {
     observer.OnRulesReady(this);
+  }
 }
 
 void DebounceComponentInstaller::OnComponentReady(

components/debounce/browser/core/debounce_rule.cc
@@ -1,9 +1,9 @@
-/* Copyright (c) 2021 The Brave Authors. All rights reserved.
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+// Copyright (c) 2023 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#include "brave/components/debounce/browser/debounce_rule.h"
+#include "brave/components/debounce/browser/core/debounce_rule.h"
 
 #include <memory>
 #include <string_view>

ios/browser/api/debounce/debounce_service.mm
@@ -1,0 +1,45 @@
+// Copyright (c) 2023 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "brave/ios/browser/api/debounce/debounce_service+private.h"
+
+#include <MacTypes.h>
+#include <string>
+
+#include "base/memory/raw_ptr.h"
+#include "brave/components/debounce/browser/core/debounce_service.h"
+#import "net/base/mac/url_conversions.h"
+#include "url/gurl.h"
+
+@interface DebounceService () {
+  raw_ptr<debounce::DebounceService> debounceService_;  // NOT OWNED
+}
+
+@end
+
+@implementation DebounceService
+
+- (instancetype)initWithDebounceService:
+    (debounce::DebounceService*)debounceService {
+  self = [super init];
+  if (self) {
+    debounceService_ = debounceService;
+  }
+  return self;
+}
+
+- (nullable NSURL*)debounceURL:(NSURL*)url {
+  DCHECK(debounceService_);
+  GURL gurl = net::GURLWithNSURL(url);
+  GURL final_url;
+
+  if (debounceService_->Debounce(gurl, &final_url)) {
+    return net::NSURLWithGURL(final_url);
+  }
+
+  return nullptr;
+}
+
+@end

fulldiveVR/e-browser-core

browser/brave_content_browser_client.cc
@@ -65,7 +65,7 @@
 #include "brave/components/cosmetic_filters/browser/cosmetic_filters_resources.h"
 #include "brave/components/cosmetic_filters/common/cosmetic_filters.mojom.h"
 #include "brave/components/de_amp/browser/de_amp_throttle.h"
-#include "brave/components/debounce/browser/debounce_navigation_throttle.h"
+#include "brave/components/debounce/content/browser/debounce_navigation_throttle.h"
 #include "brave/components/decentralized_dns/content/decentralized_dns_navigation_throttle.h"
 #include "brave/components/google_sign_in_permission/google_sign_in_permission_throttle.h"
 #include "brave/components/google_sign_in_permission/google_sign_in_permission_util.h"

components/debounce/core/browser/debounce_component_installer.cc
@@ -1,9 +1,9 @@
-/* Copyright (c) 2021 The Brave Authors. All rights reserved.
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+// Copyright (c) 2021 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#include "brave/components/debounce/browser/debounce_component_installer.h"
+#include "brave/components/debounce/core/browser/debounce_component_installer.h"
 
 #include <memory>
 #include <utility>
@@ -18,7 +18,6 @@
 #include "base/types/expected.h"
 #include "brave/components/brave_component_updater/browser/dat_file_util.h"
 #include "brave/components/brave_component_updater/browser/local_data_files_service.h"
-#include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 
 using brave_component_updater::LocalDataFilesObserver;
 using brave_component_updater::LocalDataFilesService;
@@ -52,8 +51,9 @@ void DebounceComponentInstaller::OnDATFileDataReady(
   host_cache_.clear();
   rules_ = std::move(parsed_rules.value().first);
   host_cache_ = parsed_rules.value().second;
-  for (Observer& observer : observers_)
+  for (Observer& observer : observers_) {
     observer.OnRulesReady(this);
+  }
 }
 
 void DebounceComponentInstaller::OnComponentReady(

components/debounce/core/browser/debounce_rule.cc
@@ -1,9 +1,9 @@
-/* Copyright (c) 2021 The Brave Authors. All rights reserved.
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+// Copyright (c) 2021 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#include "brave/components/debounce/browser/debounce_rule.h"
+#include "brave/components/debounce/core/browser/debounce_rule.h"
 
 #include <memory>
 #include <optional>

ios/browser/api/debounce/debounce_service.mm
@@ -1,0 +1,45 @@
+// Copyright (c) 2023 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "brave/ios/browser/api/debounce/debounce_service+private.h"
+
+#include <MacTypes.h>
+#include <string>
+
+#include "base/memory/raw_ptr.h"
+#include "brave/components/debounce/core/browser/debounce_service.h"
+#import "net/base/mac/url_conversions.h"
+#include "url/gurl.h"
+
+@interface DebounceService () {
+  raw_ptr<debounce::DebounceService> debounceService_;  // NOT OWNED
+}
+
+@end
+
+@implementation DebounceService
+
+- (instancetype)initWithDebounceService:
+    (debounce::DebounceService*)debounceService {
+  self = [super init];
+  if (self) {
+    debounceService_ = debounceService;
+  }
+  return self;
+}
+
+- (nullable NSURL*)debounceURL:(NSURL*)url {
+  DCHECK(debounceService_);
+  GURL gurl = net::GURLWithNSURL(url);
+  GURL final_url;
+
+  if (debounceService_->Debounce(gurl, &final_url)) {
+    return net::NSURLWithGURL(final_url);
+  }
+
+  return nullptr;
+}
+
+@end
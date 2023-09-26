9f809f2adec9216bb1b4e6c2b815036160ce88c2
Jacob Sikorski <jacob.sikorski@gmail.com>
2023-09-26 12:47:23 -0600

    Add DebounceService for iOS

--- components/debounce/core/common/pref_names.h
+++ components/debounce/core/common/pref_names.h
@@ -1,10 +1,10 @@
-// Copyright (c) 2022 The Brave Authors. All rights reserved.
+// Copyright (c) 2021 The Brave Authors. All rights reserved.
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#ifndef BRAVE_COMPONENTS_DEBOUNCE_COMMON_PREF_NAMES_H_
-#define BRAVE_COMPONENTS_DEBOUNCE_COMMON_PREF_NAMES_H_
+#ifndef BRAVE_COMPONENTS_DEBOUNCE_CORE_COMMON_PREF_NAMES_H_
+#define BRAVE_COMPONENTS_DEBOUNCE_CORE_COMMON_PREF_NAMES_H_
 
 namespace debounce::prefs {
 
@@ -13,4 +13,4 @@ inline constexpr char kDebounceEnabled[] = "brave.debounce.enabled";
 
 }  // namespace debounce::prefs
 
-#endif  // BRAVE_COMPONENTS_DEBOUNCE_COMMON_PREF_NAMES_H_
+#endif  // BRAVE_COMPONENTS_DEBOUNCE_CORE_COMMON_PREF_NAMES_H_
