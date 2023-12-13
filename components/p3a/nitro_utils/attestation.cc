samartnik/brave-core

Use certificate verification logic off boringssl
All code relating to net/der, and net/cert/pki has been moved into
boringssl for a while. However, the duplicated code under //net is now
getting deleted.

This change corrects the inclusion file paths, and the namespace, to
make use of the code from boringssl.

Chromium change:
https://chromium.googlesource.com/chromium/src/+/8b76aac079cc0896a0111a499042644817d3e708

commit 8b76aac079cc0896a0111a499042644817d3e708
Author: Bob Beck <bbe@chromium.org>
Date:   Fri Nov 3 14:18:12 2023 +0000

    Reland "Change over to using BoringSSL in place of net/der and net/cert/pki"

    This reverts commit 4763ed3ead5af1850833773aa4757e264fb6bd6e.

    Reason for revert: Cronet IOS build bots have been shut down
    in https://chromium-review.googlesource.com/c/chromium/src/+/4988794

    Original change's description:
    > Revert "Change over to using BoringSSL in place of net/der and net/cert/pki"
    >
    > This reverts commit 88963e95f705d2f63114b18be9015f562fbbd20a.
    >
    > Reason for revert:
    > LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8766070381079393057
    >
    > Sample failed build: https://ci.chromium.org/b/8766070381079393057
    >
    > If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8766070381079393057&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F4917011
    >
    > Original change's description:
    > > Change over to using BoringSSL in place of net/der and net/cert/pki
    > >
    > > This changes Chrome to use the core of the X.509 verification logic
    > > from BoringSSL's libpki (w

components/p3a/nitro_utils/attestation.cc
@@ -22,7 +22,6 @@
 #include "components/cbor/reader.h"
 #include "crypto/random.h"
 #include "net/base/url_util.h"
-#include "net/cert/pki/parsed_certificate.h"
 #include "net/cert/x509_certificate.h"
 #include "net/cert/x509_util.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
@@ -32,6 +31,7 @@
 #include "services/network/url_loader_factory.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/boringssl/src/include/openssl/pool.h"
+#include "third_party/boringssl/src/pki/parsed_certificate.h"
 
 namespace nitro_utils {
 
@@ -153,7 +153,7 @@ bool VerifyUserDataKey(scoped_refptr<net::X509Certificate> server_cert,
   return false;
 }
 
-absl::optional<net::ParsedCertificateList> ParseCertificatesAndCheckRoot(
+absl::optional<bssl::ParsedCertificateList> ParseCertificatesAndCheckRoot(
     scoped_refptr<net::X509Certificate> server_cert,
     const cbor::Value::MapValue& cose_map) {
   const auto cert_it = cose_map.find(cbor::Value("certificate"));
@@ -173,19 +173,19 @@ absl::optional<net::ParsedCertificateList> ParseCertificatesAndCheckRoot(
                  std::back_inserter(cert_vals),
                  [](const cbor::Value& val) { return val.Clone(); });
 
-  net::ParsedCertificateList cert_chain;
+  bssl::ParsedCertificateList cert_chain;
 
-  net::ParseCertificateOptions parse_cert_options;
+  bssl::ParseCertificateOptions parse_cert_options;
   // Nitro enclave certs seem to contain serial numbers that Chromium does not
   // like, so we disable serial number validation
   parse_cert_options.allow_invalid_serial_numbers = true;
-  net::CertErrors cert_errors;
+  bssl::CertErrors cert_errors;
   for (auto& cert_val : cert_vals) {
     if (!cert_val.is_bytestring()) {
       LOG(ERROR) << "Nitro verification: certificate is not bstr";
       return absl::nullopt;
     }
-    if (!net::ParsedCertificate::CreateAndAddToVector(
+    if (!bssl::ParsedCertificate::CreateAndAddToVector(
             net::x509_util::CreateCryptoBuffer(cert_val.GetBytestring()),
             parse_cert_options, &cert_chain, &cert_errors)) {
       LOG(ERROR) << "Nitro verification: failed to parse certificate: "
@@ -260,7 +260,7 @@ void ParseAndVerifyDocument(
     return;
   }
 
-  absl::optional<net::ParsedCertificateList> cert_chain =
+  absl::optional<bssl::ParsedCertificateList> cert_chain =
       ParseCertificatesAndCheckRoot(server_cert, cose_map);
   if (!cert_chain.has_value()) {
     std::move(result_callback).Run(scoped_refptr<net::X509Certificate>());

fulldiveVR/e-browser-core

components/p3a/nitro_utils/attestation.cc
@@ -23,7 +23,6 @@
 #include "components/cbor/reader.h"
 #include "crypto/random.h"
 #include "net/base/url_util.h"
-#include "net/cert/pki/parsed_certificate.h"
 #include "net/cert/x509_certificate.h"
 #include "net/cert/x509_util.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
@@ -32,6 +31,7 @@
 #include "services/network/public/mojom/url_response_head.mojom.h"
 #include "services/network/url_loader_factory.h"
 #include "third_party/boringssl/src/include/openssl/pool.h"
+#include "third_party/boringssl/src/pki/parsed_certificate.h"
 
 namespace nitro_utils {
 
@@ -153,7 +153,7 @@ bool VerifyUserDataKey(scoped_refptr<net::X509Certificate> server_cert,
   return false;
 }
 
-std::optional<net::ParsedCertificateList> ParseCertificatesAndCheckRoot(
+std::optional<bssl::ParsedCertificateList> ParseCertificatesAndCheckRoot(
     scoped_refptr<net::X509Certificate> server_cert,
     const cbor::Value::MapValue& cose_map) {
   const auto cert_it = cose_map.find(cbor::Value("certificate"));
@@ -173,19 +173,19 @@ std::optional<net::ParsedCertificateList> ParseCertificatesAndCheckRoot(
                  std::back_inserter(cert_vals),
                  [](const cbor::Value& val) { return val.Clone(); });
 
-  net::ParsedCertificateList cert_chain;
+  bssl::ParsedCertificateList cert_chain;
 
-  net::ParseCertificateOptions parse_cert_options;
+  bssl::ParseCertificateOptions parse_cert_options;
   // Nitro enclave certs seem to contain serial numbers that Chromium does not
   // like, so we disable serial number validation
   parse_cert_options.allow_invalid_serial_numbers = true;
-  net::CertErrors cert_errors;
+  bssl::CertErrors cert_errors;
   for (auto& cert_val : cert_vals) {
     if (!cert_val.is_bytestring()) {
       LOG(ERROR) << "Nitro verification: certificate is not bstr";
       return std::nullopt;
     }
-    if (!net::ParsedCertificate::CreateAndAddToVector(
+    if (!bssl::ParsedCertificate::CreateAndAddToVector(
             net::x509_util::CreateCryptoBuffer(cert_val.GetBytestring()),
             parse_cert_options, &cert_chain, &cert_errors)) {
       LOG(ERROR) << "Nitro verification: failed to parse certificate: "
@@ -260,7 +260,7 @@ void ParseAndVerifyDocument(
     return;
   }
 
-  std::optional<net::ParsedCertificateList> cert_chain =
+  std::optional<bssl::ParsedCertificateList> cert_chain =
       ParseCertificatesAndCheckRoot(server_cert, cose_map);
   if (!cert_chain.has_value()) {
     std::move(result_callback).Run(scoped_refptr<net::X509Certificate>());