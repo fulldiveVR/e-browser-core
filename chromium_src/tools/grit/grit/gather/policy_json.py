a113a994c900b7c61a8e7db1137a30f92f6930a7
Managerdark <manageradrk@gmail.com>
2024-03-07 13:14:59 +0300

    Chaged brave to aiwize

--- chromium_src/tools/grit/grit/gather/policy_json.py
+++ chromium_src/tools/grit/grit/gather/policy_json.py
@@ -9,8 +9,8 @@ import override_utils
 @override_utils.override_method(PolicyJson)
 def SetDefines(self, _orig_method, _defines):
     self._config = {
-        'build': 'brave',
-        'app_name': 'Brave',
-        'frame_name': 'Brave Frame',
+        'build': 'aiwize',
+        'app_name': 'AIWize',
+        'frame_name': 'AIWize Frame',
         'os_name': 'Google Chrome OS'
     }
